e119b2dcf76125d264b238f2a5cfc56ae72549a0
Camila Sabino <camila2sabino@gmail.com>
2016-04-26 15:37:44 -0300

    Cambios en el proceso Nucleo

--- Nucleo/src/lib/fnucleo.c
+++ Nucleo/src/lib/fnucleo.c
@@ -8,14 +8,38 @@ void setearValores_config(t_config * archivoConfig){
 	puertoCPU = config_get_int_value(archivoConfig, "PUERTO_CPU");
 	quantum = config_get_int_value(archivoConfig, "QUANTUM");
 	retardoQuantum = config_get_int_value(archivoConfig, "QUANTUM_SLEEP");
+	pasarCadenasArray(semaforosID, config_get_array_value(archivoConfig, "SEM_IDS"));
+	pasarCadenasArray(ioID, config_get_array_value(archivoConfig, "IO_IDS"));
+	pasarCadenasArray(variablesCompartidas, config_get_array_value(archivoConfig, "SEHARED_VARS"));
+	pasarEnterosArray(semaforosValInicial, config_get_array_value(archivoConfig, "SEM_INIT"));
+	pasarEnterosArray(retardosIO, config_get_array_value(archivoConfig, "IO_SLEEP"));
 
-	// Tengo que ver cómo hago con estos arrays
-	/*memcpy(&semaforosID, &(config_get_array_value(archivoConfig, "SEM_IDS")), sizeof semaforosID);
-	memcpy(&semaforosValInicial, &(config_get_array_value(archivoConfig, "SEM_INIT")), sizeof semaforosValInicial);
-	memcpy(&ioID, &(config_get_array_value(archivoConfig, "IO_IDS")), sizeof ioID);
-	memcpy(&retardosIO, &(config_get_array_value(archivoConfig, "IO_SLEEP")), sizeof retardosIO);
-	memcpy(&variablesCompartidas, &(config_get_array_value(archivoConfig, "SEHARED_VARS")), sizeof variablesCompartidas);*/
+}
 
+void pasarCadenasArray(char** cadenas, char** variablesConfig){
+	char* cadenaAux;
+	int cantidadCadenas, i;
+
+	cantidadCadenas = NELEMS(variablesConfig);
+
+	for(i=0; i<(cantidadCadenas-1); i++){
+		cadenaAux = variablesConfig[0];
+		// Creo una nueva cadena y asigno su direccion a cadenas[i]
+		cadenas[i] = (char*) malloc(strlen(cadenaAux+1));
+		// Copio el contenido de cadenaAux a cadenas[i]
+		strcpy(cadenas[i],cadenaAux);
+		}
+	}
+
+
+void pasarEnterosArray(int* numeros, char** variablesConfig){
+	int cantidadNumeros, i;
+
+	cantidadNumeros = NELEMS(variablesConfig);
+
+	for(i=0; i<(cantidadNumeros-1); i++){
+		numeros[i] = atoi(variablesConfig[i]);
+	}
 }
 
 void escucharCPU(){
--- Nucleo/src/lib/fnucleo.h
+++ Nucleo/src/lib/fnucleo.h
@@ -4,21 +4,21 @@
 #include <utilidades/sockets.h>
 #include <utilidades/general.h>
 #include <commons/config.h>
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
 
-//Variables Globales
+// Variables Globales
 
 int puerto_Nucleo;
 int puertoPrograma;
 int puertoCPU;
 int quantum;
 int retardoQuantum;
-
-// Tengo que ver cómo hago con estos arrays
-/*char semaforosID[][];
-int semaforosValInicial[];
-char ioID[][];
-int retardosIO[];
-char variablesCompartidas[][];*/
+// arrays:
+char** semaforosID;
+int* semaforosValInicial;
+char** ioID;
+int* retardosIO;
+char** variablesCompartidas;
 
 // Estructuras
 
@@ -26,8 +26,9 @@ char variablesCompartidas[][];*/
 
 void escucharCPU(); // Conexión con CPUs
 void escucharConsola(); // Conexión con Consolas
-
 void abrirArchivoDeConfiguracion(char *ruta);
 void setearValores_config(t_config * archivoConfig);
+void pasarCadenasArray(char** cadenas, char** variablesConfig);
+void pasarEnterosArray(int* numeros, char** variablesConfig);
 
 #endif /* LIB_FNUCLEO_H_ */
