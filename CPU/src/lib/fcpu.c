eef9de04b3ab24cd0729901c6a3ab5b3052c5d3b
LucasGiani <LucasGiani_05@outlook.com.ar>
2016-04-30 00:41:27 -0300

    mergear

--- CPU/src/lib/fcpu.c
+++ CPU/src/lib/fcpu.c
@@ -14,40 +14,16 @@ void conectarConNucleo(){
 int fd_serverCPU;
 
 	fd_serverCPU = nuevoSocket();
-	asociarSocket(fd_serverCPU, puertoNucleo);
 	conectarSocket(fd_serverCPU, ipNucleo, puertoNucleo);
-	// Creo un paquete (string) de size PACKAGESIZE, que le enviaré al Núcleo
-	int enviar = 1;
-		char message[PACKAGESIZE];
-
-		printf("Conectado al Núcleo. Ya se puede enviar mensajes. Escriba 'exit' para salir\n");
-
-		while(enviar){
-			fgets(message, PACKAGESIZE, stdin);	// Lee una línea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE
-			if (!strcmp(message,"exit\n")) enviar = 0; // Chequeo que no se quiera salir
-			if (enviar) enviarPorSocket(fd_serverCPU, message, strlen(message) + 1); // Sólo envío si no quiere salir
-		}
-		close(fd_serverCPU);
+	handshake_cliente(fd_serverCPU, "C");
 } // Soy cliente del Núcleo, es  decir, soy el que inicia la conexión con él
 
 void conectarConUMC(){
 int fd_serverCPU;
 
 	fd_serverCPU = nuevoSocket();
-	asociarSocket(fd_serverCPU, puertoUMC);
 	conectarSocket(fd_serverCPU, ipUMC, puertoUMC);
-	// Creo un paquete (string) de size PACKAGESIZE, que le enviaré a la UMC
-	int enviar = 1;
-		char message[PACKAGESIZE];
-
-		printf("Conectado a la UMC. Ya se puede enviar mensajes. Escriba 'exit' para salir\n");
-
-		while(enviar){
-			fgets(message, PACKAGESIZE, stdin);	// Lee una línea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE
-			if (!strcmp(message,"exit\n")) enviar = 0; // Chequeo que no se quiera salir
-			if (enviar) enviarPorSocket(fd_serverCPU, message, strlen(message) + 1); // Sólo envío si no quiere salir
-		}
-		close(fd_serverCPU);
+	handshake_cliente(fd_serverCPU, "C");
 } // Soy cliente de la UMC, es  decir, soy el que inicia la conexión con ella
 
 void testLecturaArchivoDeConfiguracion(){
