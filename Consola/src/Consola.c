3f86197586017572bdcf310a4f3e42515a3834c0
Norman Perrin <norman.perrin.94@gmail.com>
2016-07-09 14:47:34 -0300

    Commit chico de revisión

--- Consola/src/Consola.c
+++ Consola/src/Consola.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
 	system("clear");
 	setvbuf(stdout, NULL, _IONBF, 0);
 
-	//	validar_argumentos(argc); // Se le debe ingresar la ruta del script. Aunque se ejecute por #!
+	validar_argumentos(argc);
 
 	crearLoggerConsola();
 
