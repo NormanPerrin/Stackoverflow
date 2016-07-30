f787652ba4a025b86297d96b7e4aa7c1615b8f13
lucianoperez <luciano.v.perez@hotmail.com>
2016-07-30 10:42:40 -0300

    agrego Debugs

---
+++ Nucleo/Debug/src/Nucleo.d
@@ -0,0 +1,20 @@
+src/Nucleo.d: ../src/Nucleo.c ../src/lib/principales.h \
+ ../src/lib/funciones.h ../src/lib/globales.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/comunicaciones.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/general.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/sockets.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/tiposDeDatos.h
+
+../src/lib/principales.h:
+
+../src/lib/funciones.h:
+
+../src/lib/globales.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/comunicaciones.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/general.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/sockets.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/tiposDeDatos.h:
