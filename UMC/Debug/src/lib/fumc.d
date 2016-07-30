0e26195a3dfd788941ec6b1000cd3f9f25c82009
lucianoperez <luciano.v.perez@hotmail.com>
2016-07-30 11:25:54 -0300

    GSFKNAMKNL

---
+++ UMC/Debug/src/lib/fumc.d
@@ -0,0 +1,15 @@
+src/lib/fumc.d: ../src/lib/fumc.c ../src/lib/fumc.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/comunicaciones.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/general.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/sockets.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/tiposDeDatos.h
+
+../src/lib/fumc.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/comunicaciones.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/general.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/sockets.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/tiposDeDatos.h:
