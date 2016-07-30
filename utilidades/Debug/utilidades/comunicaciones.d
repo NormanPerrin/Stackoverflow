3dde5b939d3e185d1d31a040fa1a7fdbd8696bdb
lucianoperez <luciano.v.perez@hotmail.com>
2016-07-30 10:45:23 -0300

    agrego Debug utilidades

---
+++ utilidades/Debug/utilidades/comunicaciones.d
@@ -0,0 +1,11 @@
+utilidades/comunicaciones.d: ../utilidades/comunicaciones.c \
+ ../utilidades/comunicaciones.h ../utilidades/general.h \
+ ../utilidades/sockets.h ../utilidades/tiposDeDatos.h
+
+../utilidades/comunicaciones.h:
+
+../utilidades/general.h:
+
+../utilidades/sockets.h:
+
+../utilidades/tiposDeDatos.h:
---
+++ utilidades/Debug/utilidades/general.d
@@ -0,0 +1,8 @@
+utilidades/general.d: ../utilidades/general.c ../utilidades/general.h \
+ ../utilidades/sockets.h ../utilidades/tiposDeDatos.h
+
+../utilidades/general.h:
+
+../utilidades/sockets.h:
+
+../utilidades/tiposDeDatos.h:
