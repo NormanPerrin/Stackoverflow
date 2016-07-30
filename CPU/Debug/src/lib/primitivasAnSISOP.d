72b5d97c80dd60ab9eaee9f6f04a4d21cb70d8b3
LucasGiani <LucasGiani_05@outlook.com.ar>
2016-07-29 21:56:56 -0300

    agrego debugs

---
+++ CPU/Debug/src/lib/primitivasAnSISOP.d
@@ -0,0 +1,21 @@
+src/lib/primitivasAnSISOP.d: ../src/lib/primitivasAnSISOP.c \
+ ../src/lib/primitivasAnSISOP.h ../src/lib/globalesCPU.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/comunicaciones.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/general.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/sockets.h \
+ /home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/tiposDeDatos.h \
+ ../src/lib/principalesCPU.h
+
+../src/lib/primitivasAnSISOP.h:
+
+../src/lib/globalesCPU.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/comunicaciones.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/general.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/sockets.h:
+
+/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades/utilidades/tiposDeDatos.h:
+
+../src/lib/principalesCPU.h:
---
+++ Consola/Debug/src/lib/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/lib/fconsola.c 
+
+OBJS += \
+./src/lib/fconsola.o 
+
+C_DEPS += \
+./src/lib/fconsola.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/lib/%.o: ../src/lib/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/tp-2016-1c-Cazadores-de-cucos/utilidades" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
